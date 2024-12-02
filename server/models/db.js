bfdea6a8eef1cca4f66b630482c7529a7d7403f4
antoinebrt <antoine.breteau@edu.devinci.fr>
2024-12-02 18:23:42 +0100

    Update db.js

--- server/models/db.js
+++ server/models/db.js
@@ -1,2 +1,11 @@
 const { Sequelize } = require("sequelize");
 
+const DbUrl = "mysql://user_a_definir:password_a_definir@localhost:3306/nomBd";
+
+const connection = new Sequelize(
+    process.env.DATABASE_URL ?? DbUrl
+);
+
+connection.authenticate().then(() => console.log("Database is ready"));
+
+module.exports = connection;
