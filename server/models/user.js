88c7245f4b749dfda32a96852d13fb1a77298100
P3ast <88151628+P3ast@users.noreply.github.com>
2024-12-05 19:13:22 +0100

    Create user.js

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+//const sequelize = require('CheminVersDATABASE');
+// Faut connecter a la bdd quand elle est finis  !
+// en théorie ça devrait marcher avec les bons require
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'user'),
+    defaultValue: 'user'
+  }
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  }
+});
+
+User.prototype.validatePassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
