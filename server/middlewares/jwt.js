fbde0a629930fc0cae15a963e5f2f87cc8496612
P3ast <88151628+P3ast@users.noreply.github.com>
2024-12-13 15:43:22 +0100

    Create jwt.js

---
+++ server/middlewares/jwt.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secretkey' ;
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, JWT_SECRET);
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, JWT_SECRET);
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+  JWT_SECRET
+};
